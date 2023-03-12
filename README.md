d5ce8531722db369dc388eba9c7370097eb7cf74
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2023-03-12 20:10:21 +0000

    Kahoot solver

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# kahoot-solver
-Get all the answers from a kahoot
+# Kahoot-solver
+
+## Get all the answers from a kahoot
+
+To use this program you will need the id of the kahoot. This can be found in the url before starting the game. For example : "https://play.kahoot.it/v2/?quizId=f1d9f798-0a68-4eda-bd09-c41c1c2d88db" the id is : {f1d9f798-0a68-4eda-bd09-c41c1c2d88db}
+
+To run the program just use:
+
+```bash
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,33 @@
+import urllib.request as ur
+import json as j
+
+
+def get_answers(id):  
+    url = "https://play.kahoot.it/rest/kahoots/"+id
+
+    color_list = ["red", "blue", "yellow", "green"]
+    json = j.loads(ur.urlopen(url).read())
+    
+    #print title
+    print(f"\n{json.get('title')}\n")
+
+    questions = json.get("questions")
+
+    for index, slide in enumerate(questions):
+        print(f"{index+1}. {slide['question']}\n")
+        correct_choice_index = -1
+        for i in range(len(slide.get("choices"))):
+            if slide["choices"][i]["correct"]:
+                correct_choice_index = i
+                print(f"({chr(65+i)}) {slide['choices'][i].get('answer')} [CORRECT]")
+                break
+        print()
+        for i in range(len(slide.get("choices"))):
+            if i == correct_choice_index:
+                continue
+            print(f"({chr(65+i)}) {slide['choices'][i].get('answer')}")
+        print()
+
+if __name__ == "__main__":
+    get_answers(input("[-] QUIZ ID: "))
+       
